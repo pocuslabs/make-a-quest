8e365a08a3726b5da5b7d40e47687c093ecd6447
Austin Pocus <ajpocus@gmail.com>
2021-08-25 16:39:50 -0400

    move classes from component to type file

--- app/types.ts
+++ app/types.ts
@@ -1,3 +1,60 @@
+export const NOWHERE = -1;
+
+enum EntityType {
+	NPC,
+	Item
+};
+
+class Location {
+	// a Location's x and y refer to the tile where an Entity can be found
+	constructor(x = NOWHERE, y = NOWHERE) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Entity {
+	constructor(name, location) {
+		if (!name) {
+			throw new Error("You must pass a unique name to the NPC constructor.");
+		}
+
+		if (!location) {
+			location = new Location();
+		}
+
+		this.name = name;
+		this.location = location;
+	}
+}
+
+class NPC extends Entity {
+	constructor(name, location, { dialogue }) {
+		super(name, location);
+		this.dialogue = dialogue;
+	}
+}
+
+class Item extends Entity {
+	constructor(name, location, { properties }) {
+		super(name, location);
+		this.properties = properties;
+	}
+}
+
+class ItemProperties {
+	constructor(item, { })
+}
+
+class QuestComponent {
+	constructor(componentType) {
+		if (!componentType) {
+			throw new Error("You must pass a componentType parameter to the QuestComponent constructor.");
+		}
+
+		this.componentType = componentType;
+	}
+}
 export enum QuestComponentType {
 	Talk,
 	Fetch,
--- components/QuestComponent.tsx
+++ components/QuestComponent.tsx
@@ -2,64 +2,6 @@ import { useDrag } from 'react-dnd'
 
 const ITEM_TYPE = "questComponent";
 
-const EntityType = {
-	NPC: "NPC",
-	Item: "Item"
-};
-
-const NOWHERE = -1;
-
-class Location {
-	// a Location's x and y refer to the tile where an Entity can be found
-	constructor(x = NOWHERE, y = NOWHERE) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
-class Entity {
-	constructor(name, location) {
-		if (!name) {
-			throw new Error("You must pass a unique name to the NPC constructor.");
-		}
-
-		if (!location) {
-			location = new Location();
-		}
-
-		this.name = name;
-		this.location = location;
-	}
-}
-
-class NPC extends Entity {
-	constructor(name, location, { dialogue }) {
-		super(name, location);
-		this.dialogue = dialogue;
-	}
-}
-
-class Item extends Entity {
-	constructor(name, location, { properties }) {
-		super(name, location);
-		this.properties = properties;
-	}
-}
-
-class ItemProperties {
-	constructor(item, { })
-}
-
-class QuestComponent {
-	constructor(componentType) {
-		if (!componentType) {
-			throw new Error("You must pass a componentType parameter to the QuestComponent constructor.");
-		}
-
-		this.componentType = componentType;
-	}
-}
-
 const ComponentTemplates = {
 	// talk to a specific NPC
 	Talk: {
