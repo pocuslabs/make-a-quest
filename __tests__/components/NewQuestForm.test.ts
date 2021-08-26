764e041a58050ae6033fa7183b449242eb3ba2e7
Austin Pocus <ajpocus@gmail.com>
2021-08-26 16:31:23 -0400

    Scaffolding for Jest with Typescript + Babel

---
+++ __tests__/components/NewQuestForm.test.ts
@@ -0,0 +1,8 @@
+describe("NewQuestForm", () => {
+    test("creates a quest for a story", () => {
+        expect(true).toBeTruthy();
+    });
+
+    test.todo("the quest has at least one component");
+    test.todo("shows an error message when the story doesn't exist");
+});
